986e37fdd8bac52bbd8cb8a4b1fad6b0f5beb39a
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-01-21 18:40:39 -0800

    Lock thread at start of namespace unit tests
    - keeps things green
    
    [#112080719]

--- lib/namespace/namespace_suite_test.go
+++ lib/namespace/namespace_suite_test.go
@@ -1,6 +1,8 @@
 package namespace_test
 
 import (
+	"runtime"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -8,6 +10,7 @@ import (
 )
 
 func TestNamespace(t *testing.T) {
+	runtime.LockOSThread()
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Namespace Suite")
 }
